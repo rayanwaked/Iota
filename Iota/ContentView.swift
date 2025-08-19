c464d06fc0154270e28a80a0606034f933ff8da6
Rayan Waked <rayan@waked.dev>
2025-08-19 11:54:11 -0700

    Initial Commit

---
+++ Iota/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Iota
+//
+//  Created by Rayan Waked on 8/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Iota/IotaApp.swift
@@ -0,0 +1,17 @@
+//
+//  IotaApp.swift
+//  Iota
+//
+//  Created by Rayan Waked on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct IotaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
